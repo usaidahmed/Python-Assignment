5d4b1ea225994bd57ff23dfc63459b35c4b8e2df
usaidahmed <50077112+usaidahmed@users.noreply.github.com>
2019-05-08 16:53:25 +0500

    Add files via upload

---
+++ assignment_1 06-05-19.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # ASSIGNMENT 
+
+# <h3> 1 : Declare and initialize a variable name total_marks with a value 500</h3>
+# 
+
+# In[18]:
+
+
+Total_Marks=500
+
+
+# <h3> 2 : Declare and initialize 5 variables name maths , english , urdu , physics and chemisrty with different integer values</h3>
+
+# In[20]:
+
+
+Maths=62
+English=60
+Urdu=58
+Physics=65
+Chemistry=57
+
+
+# <h3>3 : Calculate the obtained marks And assign the value In variable name obtained_marks</h3>
+
+# In[41]:
+
+
+Maths=62
+English=60
+Urdu=60
+Physics=65
+Chemistry=57
+(62+60+60+65+57)
+"Obtained_Marks=304"
+
+
+# <h3>4 : Calculate percentage and assign the value in variable name percentage</h3>
+
+# In[43]:
+
+
+Obtained_Marks=304
+Total_Marks=500
+(304/500)*100
+"Percentage=60.8"
+
+
+# <h3>5 : Declare and initialize an empty variable name grade </h3>
+
+# In[22]:
+
+
+Grade=None
+
+
+# ### 6: print your biodata 
+
+# In[27]:
+
+
+print("Name:Usaid Ahmed")
+
+
+# In[31]:
+
+
+print("Age=18")
+
+
+# In[37]:
+
+
+print("Education:College student")
+
+
+# In[39]:
+
+
+print("Date of Birth:04-04-2000")
+
+
+# In[41]:
+
+
+print("Religion:Islam")
+
+
+# In[42]:
+
+
+print("Nationality:Paksitan")
+
+
+# In[ ]:
+
+
+
+
